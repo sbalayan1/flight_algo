90e3372617affc288ceb5f819d28acf7444aaae6
sbalayan1 <balayans2014@yahoo.com>
2022-08-12 21:56:45 -0700

    update

--- algo.py
+++ algo.py
@@ -12,13 +12,41 @@ graph = {
         "London": ["Berlin", "Egypt"],
         "Iceland": ["London"],
         'Berlin': ["Paris"],
-        "Paris": ["Amsterdam"],
+        "Paris": ["London", "Amsterdam"],
         "Amsterdam": [],
         "Egypt": []
     }
 
-
 def find_paths(graph, start, destination):
+    queue = [[start]]
+    paths = []
+
+    while queue:
+        node = queue.pop(0)
+
+        for neighbor in graph[node[-1]]:
+            if node[-1] == start:
+                queue.append([start, neighbor])
+            else:
+                node.append(neighbor)
+                if destination != neighbor:
+                    queue.append(node)
+                else:
+                    paths.append(node)
+
+    return paths if len(paths) > 0 else None
+
+
+print(find_paths(graph, "NY", "Paris"))
+
+
+#node = [NY], queue = []
+#node = [NY], queue = [[NY, Iceland], [NY, Maine]] #neighbors of newyork are Iceland and Maine
+#node = [NY, Iceland], queue = [[NY, Maine], [NY, Iceland, London]] #neighbor of Iceland is London
+#node = [NY, Maine], queue = [NY, Iceland, London], [NY, Maine, London] #neighbor of Maine is London
+
+
+def find_paths_stack_dfs(graph, start, destination):
     # if graph.get(start) is None or graph.get(destination) is None: return None
     stack = [[start]]
     # visited.add(start)
@@ -40,14 +68,11 @@ def find_paths(graph, start, destination):
                 # stack = []
                 break
         
-
-
     return paths if len(paths) > 0 else None
 
 
-print(find_paths(graph, "NY", "Egypt")) 
+# print(find_paths(graph, "NY", "Egypt")) 
 #[['NY', 'Maine', 'London'], ['NY', 'Iceland', 'London'], ['NY', 'Paris', 'London']]
-
 #node = [NY], stack = [[NY, Iceland], [NY, Maine]]
 #node = [NY, Maine], stack = [[NY, Iceland]]
 
