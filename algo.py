b7806c0e31a0521070afd224fc207daea05de0c2
sbalayan1 <balayans2014@yahoo.com>
2022-08-30 14:02:38 -0700

    update

--- algo.py
+++ algo.py
@@ -1,14 +1,35 @@
-import collections
-from pydoc import pathdirs
-
 # flights  =  "NY -> Iceland -> London -> Berlin, \
 #     NY -> Maine -> London, \
 #     Berlin -> Paris -> Amsterdam, \
 #     Paris -> London -> Egypt"
 
+# start = ny, end = egypt
+# visited = {ny,iceland,london}
+# result = [ny-iceland-london-egypt, ny-maine-london-egypt]
+# queue = [ny-iceland-london-berlin-paris-amsterdam, ny-maine-london-berlin-paris-amsterdam]
+# 
+
+#don't want to add london or berlin yet because other nodes may need to visit london/berlin
+#maybe after we process the entire queue? we can add nodes to our set?
+
+
+
+#algo currently works for single paths but doesn't work if a deeper node has multiple paths to a given node. For instance, our algorithm returns [NY, Iceland, Paris, London, Egypt] if given NY and Egypt as destinations. However it does not capture [NY, Iceland, London, Egypt] or even [NY Iceland Berlin Paris London Egypt]
+
+# if len(node[-1]) > 1: loop through each edge and append a copy to the queue
+#     copy_node = node.copy()
+#     copy_node.append(neighbor)
+#     queue.append(copy_node)
+
+# if not graph[node[-1]] and node != destination: 
+#     #don't add to queue
+# if node == destination: 
+#     #append to result and don't add back to queue
+
+#add duplicates to the queue for the number of edges of a given node
+
 graph = {
-        "NY": ["Iceland", "Maine", "Jordan", "Egypt"],
-        "Jordan": [],
+        "NY": ["Iceland", "Maine"],
         "Maine": ["London"],
         "London": ["Berlin", "Egypt"],
         "Iceland": ["London"],
@@ -19,36 +40,111 @@ graph = {
     }
 
 
-# arr = [ny, maine, london]
-
-# berlin
-# iceland
-# stack
+flights  =  "NY -> Iceland -> London -> Berlin NY -> Maine -> London Berlin -> Paris -> Amsterdam Paris -> London -> Egypt Maine -> London -> Berlin Maine -> Berlin Iceland -> Berlin"
 
 
-def test_dfs(graph, start, destination):
-    arr = []
-    stack = [start]
-    visited = set(start)
+# pointer = false
+#iterate through the string
+    #if element is a string:
+        #append to string
+        #pointer = false
 
-    while stack:
-        node = stack.pop()
-        #if a start/destination doesn't have a path
-        #if a node has no edges and is not the destination:
-        if node != destination and graph[node]:
-            arr.append(node)
+    #if space
+        # if currentIndex + 1 == "-" and pointer == false
+            #append string to queue
+            #string ""
+        #     pointer = true
+        # if currentIndex + 1 == string and pointer = false
+            #append string to queue
+            #string = ""
+            #process queue until empty
+        
+        # if element == str[-1]:
+        #     append string to queue
+        #     process queue until empty
+        
 
-        if node == destination:
-            arr.append(node)
-            return arr
+def convert_string(string):
+    result = {}
+    pointer = False
+    queue = []
+    city = ""
 
-        for neighbor in graph[node]:
+    for index, char in enumerate(string):
+        if char.isalpha():
+            city += char
+            pointer = False
+
+        if char == " ":
+            if string[index+1] == "-" and pointer is False:
+                queue.append(city)
+                city = ""
+                pointer = True
+
+            if string[index+1].isalpha() and pointer is False:
+                queue.append(city)
+                city = ""
+                process_queue(queue, result)
+        
+        if index == len(string)-1: 
+            queue.append(city)
+            process_queue(queue, result)
+
+    return result
+
+def process_queue(queue, graph):
+    origin = None
+
+    while queue:
+        city = queue.pop(0)
+        if not origin: 
+            origin = city
+        else:
+            if origin not in graph:
+                graph[origin] = [city]
+            
+            if city not in graph[origin]:
+                graph[origin].append(city)
+
+            origin = city
+
+    if origin is not None and origin not in graph:
+        graph[origin] = []
+        origin = None
+
+    return graph
+
+print(convert_string(flights))
+
+def find_paths_bfs(graph, start, destination):
+    result = []
+    queue = [[start]]
+
+    while queue:
+        node = queue.pop(0)
+        visited = set(node)
+
+        if node[-1] == destination:
+            result.append(node)
+            continue
+        
+        if not graph[node[-1]] and node != destination:
+            continue
+            
+        for neighbor in graph[node[-1]]:
             if neighbor not in visited:
-                stack.append(neighbor)
-                visited.add(neighbor)
+                copy_node = node.copy()
+                copy_node.append(neighbor)
+                queue.append(copy_node)        
 
-    return False
-# paths = [[NY, Maine, London]]
+    return result
+
+# print(find_paths_bfs(graph, "NY", "Paris"))
+
+
+
+
+
 
 def find_paths(graph, start, destination):
     paths = []
@@ -84,78 +180,4 @@ def has_path(graph, start, neighbor, destination, paths):
 
     return False
         
-print(find_paths(graph, "NY", "Egypt"))
-    # return False
-
-    #may need something to notify if a path was found or not
-
-
-# print(find_paths(graph, "NY", "Maine"))
-
-# def find_paths(graph, start, destination):
-#     queue = [[start]]
-#     paths = []
-#     visited = set()
-
-#     while queue:
-#         node = queue.pop(0)
-#         # if node[-1] == destination: break
-#         for neighbor in graph[node[-1]]:
-#             if node[-1] == start:
-#                 queue.append([start, neighbor])
-#             else:
-#                 # if len(graph[node[-1]]) > 1:
-#                     # start = NY, Destination = Paris
-#                     # in cases where a node has multiple edges, we have to add to the queue like so:
-#                     # [NY, Iceland, London, Berlin] and [NY, Iceland, London, Berlin]. Instead we 
-
-#                 if neighbor != destination:
-#                     node.append(neighbor)
-#                     queue.append(node)
-#                 else:
-#                     node.append(neighbor)
-#                     paths.append(node)
-#                     break
-
-#     return paths if len(paths) > 0 else None
-
-
-# print(find_paths(graph, "NY", "Berlin"))
-
-
-#node = [NY], queue = []
-#node = [NY], queue = [[NY, Iceland], [NY, Maine]] #neighbors of newyork are Iceland and Maine
-#node = [NY, Iceland], queue = [[NY, Maine], [NY, Iceland, London]] #neighbor of Iceland is London
-#node = [NY, Maine], queue = [NY, Iceland, London], [NY, Maine, London] #neighbor of Maine is London
-
-
-def find_paths_stack_dfs(graph, start, destination):
-    # if graph.get(start) is None or graph.get(destination) is None: return None
-    stack = [[start]]
-    # visited.add(start)
-    paths = []
-    while stack:
-        node = stack.pop()
-        if node[-1] == destination: continue #breaking because we are skipping the destination node
-
-        for neighbor in graph[node[-1]]:
-            if node[-1] == start:
-                stack.append([start, neighbor])
-            else: 
-                node.append(neighbor) 
-                stack.append(node)
-
-            if neighbor == destination:
-                paths.append(stack[-1])
-                # stack = stack.pop()
-                # stack = []
-                break
-        
-    return paths if len(paths) > 0 else None
-
-
-# print(find_paths(graph, "NY", "Egypt")) 
-#[['NY', 'Maine', 'London'], ['NY', 'Iceland', 'London'], ['NY', 'Paris', 'London']]
-#node = [NY], stack = [[NY, Iceland], [NY, Maine]]
-#node = [NY, Maine], stack = [[NY, Iceland]]
-
+# print(find_paths(graph, "NY", "Egypt"))
